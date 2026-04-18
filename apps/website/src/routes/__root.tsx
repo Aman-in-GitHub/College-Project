a45c0ef8a9ee820e17f627d9a967b323e6b703fe
Aman <amanchandinc@gmail.com>
2026-04-18 19:22:55 +0545

    fix: make logo a link

--- apps/website/src/routes/__root.tsx
+++ apps/website/src/routes/__root.tsx
@@ -3,6 +3,7 @@ import type { QueryClient } from "@tanstack/react-query";
 import { ProgressProvider, useProgress } from "@bprogress/react";
 import {
   HeadContent,
+  Link,
   Outlet,
   createRootRouteWithContext,
   useRouterState,
@@ -41,9 +42,9 @@ function RootLayout() {
       <HeadContent />
       <ProgressProvider color="#432dd7" height="4px">
         <NavigationProgress />
-        <div className="flex items-center justify-center">
+        <Link to="/" className="flex w-full items-center justify-center">
           <img src="/favicon.svg" alt="College Project Logo" className="size-32" />
-        </div>
+        </Link>
         <Outlet />
         <Toaster position="top-center" duration={5000} visibleToasts={3} richColors={true} />
       </ProgressProvider>
